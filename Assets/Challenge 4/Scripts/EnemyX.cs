f7237e5e7b3bd1ac5e82cc78a54f45e2f4641969
Alper Köşgeroğlu <akoskarolu8@gmail.com>
2023-06-03 09:46:39 +0300

    1.0

---
+++ Assets/Challenge 4/Scripts/EnemyX.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyX : MonoBehaviour
+{
+    public float speed = 50f;
+  
+    private Rigidbody enemyRb;
+    private GameObject playerGoal;
+
+   
+    void Start()
+    {
+        playerGoal = GameObject.Find("Player Goal");
+        enemyRb = GetComponent<Rigidbody>();
+    }
+
+   
+    void Update()
+    {
+        Vector3 lookDirection = (playerGoal.transform.position - transform.position).normalized;
+        enemyRb.AddForce(speed * Time.deltaTime * lookDirection);
+
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.name == "Enemy Goal")
+        {
+            Destroy(gameObject);
+        } 
+        else if (other.gameObject.name == "Player Goal")
+        {
+            Destroy(gameObject);
+        }
+
+    }
+
+}
---
+++ Assets/Challenge 4/Scripts/SpawnManagerX.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManagerX : MonoBehaviour
+{
+    EnemyX EnemyXenemy;
+    [SerializeField] private GameObject enemyPrefab;
+    [SerializeField] private GameObject powerupPrefab;
+
+    readonly private float spawnRangeX = 10;
+    readonly private float spawnZMin = 15;
+    readonly private float spawnZMax = 25;
+
+    [SerializeField] private int enemyCount;
+    [SerializeField] private GameObject player;
+    private int waveCount = 1;
+    private void Start()
+    {
+        EnemyXenemy = GameObject.FindAnyObjectByType<EnemyX>();
+    }
+    void Update()
+    {
+        enemyCount = GameObject.FindGameObjectsWithTag("Enemy").Length;
+
+        if (enemyCount == 0)
+        {
+            SpawnEnemyWave(waveCount);
+        }
+    }
+
+    Vector3 GenerateSpawnPosition()
+    {
+        float xPos = Random.Range(-spawnRangeX, spawnRangeX);
+        float zPos = Random.Range(spawnZMin, spawnZMax);
+        return new Vector3(xPos, 0, zPos);
+    }
+
+
+    void SpawnEnemyWave(int enemiesToSpawn)
+    {
+        Vector3 powerupSpawnOffset = new(0, 0, -15);
+
+        if (GameObject.FindGameObjectsWithTag("Powerup").Length == 0)
+        {
+            Instantiate(powerupPrefab, GenerateSpawnPosition() + powerupSpawnOffset, powerupPrefab.transform.rotation);
+        }
+
+
+        for (int i = 0; i < waveCount; i++)
+        {
+            Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+        }
+
+        waveCount++;
+
+       // EnemyXenemy.speed += 10;
+        ResetPlayerPosition();
+
+    }
+
+    void ResetPlayerPosition()
+    {
+        player.transform.position = new Vector3(0, 1, -7);
+        player.GetComponent<Rigidbody>().velocity = Vector3.zero;
+        player.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+
+    }
+
+}
